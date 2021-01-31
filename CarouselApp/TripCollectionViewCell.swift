8eab25dd6fb822af487f93aeb28a6f8b4e778307
Dmitriy Kara <kara@group-ib.com>
2021-01-31 14:58:39 +0300

    Fix image

---
+++ CarouselApp/TripCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  TripCollectionViewCell.swift
+//  CarouselApp
+//
+//  Created by Dmitriy Kara on 28.01.2021.
+//
+
+import UIKit
+
+protocol TripCollectionCellDelegate {func didLikeButtonPressed(cell: TripCollectionViewCell)}
+
+class TripCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var countryLabel: UILabel!
+    @IBOutlet var totalDaysLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    var delegate:TripCollectionCellDelegate?
+    
+    var isLiked:Bool = false {
+        didSet {
+            if isLiked {
+                likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            } else {
+                likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+            }
+        }
+    }
+    
+    @IBAction func likeButtonTapped(sender: AnyObject) {
+        delegate?.didLikeButtonPressed(cell: self)
+    }
+}
